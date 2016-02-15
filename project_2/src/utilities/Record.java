bf47666eb78d6f08d3a87e55438aec85d9b2be91
Adam Thuvesen <a.thuvesen@gmail.com>
2016-02-15 11:57:21 +0100

    Added user roles etc.

---
+++ project_2/src/utilities/Record.java
@@ -0,0 +1,37 @@
+package utilities;
+
+public class Record {
+
+	private Doctor doctor;
+	private Nurse nurse;
+	private Patient patient;
+	private String division;
+	private String medicalData;
+	private int id;
+
+	public Record(Doctor doctor, Nurse nurse, Patient patient, String division, String medicalData, int id) {
+		this.doctor = doctor;
+		this.nurse = nurse;
+		this.patient = patient;
+		this.division = division;
+		this.medicalData = medicalData;
+		this.id = id;
+	}
+	
+	public void read(User user) {
+		
+	}
+	
+	public void write(User user) {
+		
+	}
+	
+	public void create(User user) {
+		
+	}
+	
+	public void delete(User user) {
+		
+	}
+
+}
