9c60f9621313470fa25da5c3066408d579842f91
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-02-15 17:06:10 +0100

    Removed dead code from Log.

--- project_2/src/utilities/Log.java
+++ project_2/src/utilities/Log.java
@@ -1,20 +1,16 @@
 package utilities;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Calendar;
 
 public class Log {
-	private static final String logName = "action_log";
-	private static File logFile;
+	private static final String LOG_NAME = "action_log";
 	private static PrintWriter printWriter;
 	
 	public static void append(String info) {
-		logFile = new File(logName);
-
 		try {
-			printWriter = new PrintWriter(new FileWriter(logFile, true));
+			printWriter = new PrintWriter(new FileWriter(LOG_NAME, true));
 
 			printWriter.print(Calendar.getInstance().getTime().toString() + "\t");
 			printWriter.print(info + "\n");
