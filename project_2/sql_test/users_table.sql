42774d3c90fc85414d68a1de16bf8fa880e42c72
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-02-24 20:47:40 +0100

    Clean up and exported current state of DB tables for testing.

---
+++ project_2/sql_test/users_table.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.16  Distrib 10.1.11-MariaDB, for Linux (x86_64)
+--
+-- Host: puccini.cs.lth.se    Database: db142
+-- ------------------------------------------------------
+-- Server version	5.6.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users2`
+--
+
+DROP TABLE IF EXISTS `users2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users2` (
+  `username` varchar(255) DEFAULT NULL,
+  `division` varchar(255) DEFAULT NULL,
+  `permissionLevel` varchar(255) DEFAULT NULL,
+  `certNbr` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`certNbr`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users2`
+--
+
+LOCK TABLES `users2` WRITE;
+/*!40000 ALTER TABLE `users2` DISABLE KEYS */;
+INSERT INTO `users2` VALUES ('doctor','div_emergency','Doctor','13334610649522941717'),('nurse','div_rehab','Nurse','13334610649522941719'),('patient','div_rehab','Patient','13334610649522941720'),('nsa','div_rehab','Agency','13334610649522941721'),('patient2','div_rehab','Patient','13334610649522941724'),('nurse2','div_emergency','Nurse','13334610649522941725'),('nurse3','div_rehab','Nurse','13334610649522941726');
+/*!40000 ALTER TABLE `users2` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-24 20:44:24
